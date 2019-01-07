2b53a1d451cce3c02df1e6767f20a293dc03944a
nadinejackson <njackson@stuy.edu>
2019-01-07 16:59:45 -0500

    started changing server

--- basic_server.c
+++ basic_server.c
@@ -1,21 +1,45 @@
 #include "pipe_networking.h"
+#include "game.c"
 
-
+int play(char * buf, char * response, int locations[], int vals[], int * undealt)
+{
+  int chosen[3];
+  if(chosen = letters_to_vals(buf, locations, vals))
+    {
+      if (is_set(chosen))
+	{
+	  strcpy(response, "ya u did it");
+	  replace(vals, locations, chosen, undealt);
+	}
+      else
+	strcpy(response, "na, try again");
+    }
+  else
+    strcpy(response, "error");
+}
 int main() {
+  
+  int locations[15];
+  int vals[81];
+  int * undealt = 0;
   int pid = getpid();
   int to_client;
   int from_client;
   char * buf = malloc(1024 * sizeof(char));
+  char * response = malloc(1024 * sizeof(char));
   from_client = server_handshake( &to_client );
 
+  swap(vals); //initial swap
+  deal_initial(vals, locations, &undealt);
+  display(vals, locations);
   while(1)
     {
       if (getpid() - pid)
 	{
 	  if (read(from_client, buf, 1024 * sizeof(char)))
 	    {
-	      strcat(buf, ", but death is inevitable.");
-	      write(to_client, buf, 1024 * sizeof(char));
+	      play(buf, response, locations, vals, undealt);
+	      write(to_client, response, 1024 * sizeof(char));
 	    }
 	  else
 	    exit(0);
---
+++ game.c
@@ -0,0 +1,7 @@
+void swap(int vals[81]) //initialize arrays to values 0-80 in random order
+{
+  int c = 80;
+  while(c)
+    vals[c] = c; //populate with values in order
+  //randomly swap until deck is randomized
+}
