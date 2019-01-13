6fb60c47a40c50865f160eb6cd664a1a475c9ab7
nadinejackson <njackson@stuy.edu>
2019-01-13 18:07:19 -0500

    more game-like

--- basic_server.c
+++ basic_server.c
@@ -1,33 +1,38 @@
 #include "pipe_networking.h"
 #include "game.c"
-void play(char * buf, int vals[])
+void play(char * buf, int vals[], int locations[])
 {
   char temp[10];
-  strcat(buf, " yes, this is working.\n");
-  int i;
-  for (i = 0; i < 81; i++)
+  strcat(buf, " yes, this is working.\ncards:\n");
+  /*  int i;
+ for (i = 0; i < 12; i++)
     {
-      sprintf(temp, "[%d]\n", vals[i]);
+      sprintf(temp, "[%d]\n", vals[locations[i]]);
       strcat(buf, temp);
-      printf("[%d]\n", vals[i]);
-    }
+      printf("[%d]\n", vals[locations[i]]);
+      }*/
+  print_twelve(locations, vals, buf);
 }
 int main()
 {
+  int locations[15];
   int deck[81];
   int pid = getpid();
   int to_client;
   int from_client;
+  int place[1];
+  place[0] = 12;
   char * buf = malloc(1024 * sizeof(char));
   from_client = server_handshake( &to_client );
+  swap(deck);
+  deal_initial(locations);
   while(1)
     {
       if (getpid() - pid)
 	{
 	  if (read(from_client, buf, 1024 * sizeof(char)))
 	    {
-	      swap(deck);
-	      play(buf, deck);
+	      play(buf, deck, locations);
 	      write(to_client, buf, 1024 * sizeof(char));
 	    }
 	  else
--- client.c
+++ client.c
@@ -8,12 +8,9 @@ int main() {
   char * junk = malloc(sizeof(char));
   char * line = malloc(1024 * sizeof(char));
   from_server = client_handshake( &to_server );
+  write(to_server, "let's start", 1024 * sizeof(char));
   while(1)
     {
-      printf("hello. please type something and press enter.\n");
-      fscanf(stdin, "%[^\n]s", line);
-      fscanf(stdin, "%c", junk);
-      write(to_server, line, 1024 * sizeof(char));
       perror("error");
       if (read(from_server, line, 1024 * sizeof(char)))
 	printf("%s\n", line);
@@ -22,6 +19,11 @@ int main() {
 	  perror("error reading");
 	  exit(0);
 	}
+      printf("hello. please type the letters in a set and press enter.\n");
+      fscanf(stdin, "%[^\n]s", line);
+      fscanf(stdin, "%c", junk);
+      write(to_server, line, 1024 * sizeof(char));
+      
     }
   return 0;
 }
